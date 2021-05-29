4c53bc8eb5fee7e9770262a415ab6b252b171212
saurabhdaware <saurabhdaware99@gmail.com>
2021-05-29 17:56:06 +0530

    add async

---
+++ netlify/functions/content-response.js
@@ -0,0 +1,9 @@
+exports.handler = async function(event, context) {
+  return {
+      statusCode: 200,
+      body: `
+        <h2>App Heading</h2>
+        <p>Content from API</p>
+      `
+  };
+}
